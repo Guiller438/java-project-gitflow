55f445254a179c487536f4ab04b886b492ca6d47
Guillermo Alvarez <129327266+Guiller438@users.noreply.github.com>
2025-01-07 02:09:55 +0000

    Preparar version 1.0.0

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Mundo"));
+        System.out.println("Versión 1.0.0 lista para producción.");
     }
 }
